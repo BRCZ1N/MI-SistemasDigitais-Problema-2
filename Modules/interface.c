58eae90ce68b063bc3af76c974c59cc50e8b68b7
Luís Eduardo <82675992+duasck@users.noreply.github.com>
2024-11-08 10:11:34 -0300

    ATT

--- Modules/interface.c
+++ Modules/interface.c
@@ -1,51 +1,58 @@
+
 #include "prototype.h"
 #include "start.h"
 #include "over.h"
 
-void gameOverTela() {
-    renderBitmap(80, 60, gameover4frame_data, 4);
+void gameOverTela(){
+    for (int i = 0; i < 4; i++) {
+        renderBitmap(80, 60, gameover4frame_data[i]);
+    }
 }
 
-void startGameTela() {
+void startGameTela(){
     setBackgroundColor(7, 7, 7);
-    renderBitmap(80, 60, start6frames_data, 6);
+    for (int i = 0; i < 6; i++) {
+        renderBitmap(80, 60, start6frames_data[i]);
+    }
 }
 
-void renderBitmap(int width, int height, int bitmaps[][HEIGHT][WIDTH], int frames) {
-    for (int f = 0; f < frames; f++) {
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (bitmaps[f][i][j] == 1) {
-                    switch (bitmaps[f][i][j]) {
-                        case 0xff000000: // Preto 
-                            setBackgroundBlock(j, i, 0, 0, 0);
-                            break;
-                        case 0xffffffff: // Branco
-                            setBackgroundBlock(j, i, 7, 7, 7);
-                            break;
-                        case 0xff0000ff: // Vermelho
-                            setBackgroundBlock(j, i, 7, 0, 0);
-                            break;
-                        case 0xffff0000: // Azul 
-                            setBackgroundBlock(j, i, 0, 0, 7);
-                            break;
-                        case 0xffffff00: // Ciano
-                            setBackgroundBlock(j, i, 0, 7, 7);
-                            break;
-                        case 0xffff00ff: // Magenta 
-                            setBackgroundBlock(j, i, 7, 0, 7);
-                            break;
-                        case 0xff00ffff: // Amarelo
-                            setBackgroundBlock(j, i, 7, 7, 0);
-                            break;
-                        case 0xff00ff00: // Verde
-                            setBackgroundBlock(j, i, 0, 7, 0);
-                            break;
-                        default:
-                            break;
-                    }
-                }
+void renderBitmap(int width, int height, int bitmap[HEIGHT][WIDTH]) {
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (bitmap[i][j] == 1) {
+                switch (bitmap[i][j])
+            {
+            case 0xff000000: // Preto 
+                setBackgroundBlock(j, i, 0, 0, 0);
+                break;
+            case 0xffffffff: // Branco
+                setBackgroundBlock(j, i, 7, 7, 7);
+                break;
+            case 0xff0000ff: // Vermelho
+                setBackgroundBlock(j, i, 7, 0, 0);
+                break;
+            case 0xffff0000: // Azul 
+                setBackgroundBlock(j, i, 0, 0, 7);
+                break;
+            case 0xffffff00: // Ciano
+               setBackgroundBlock(j, i, 0, 7, 7);
+                break;
+            case 0xffff00ff: // Magenta 
+                setBackgroundBlock(j, i, 7, 0, 7);
+                break;
+            case 0xff00ffff: // Amarelo
+                setBackgroundBlock(j, i, 7, 7, 0);
+                break;
+            case 0xff00ff00: // Verde
+                setBackgroundBlock(j, i, 0, 7, 0);
+                break;
+            default:
+                break;
+            
             }
         }
     }
 }
+}
+
